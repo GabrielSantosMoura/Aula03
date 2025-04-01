a8c3addd23d00e5d3c2c346704f8227dbe8ea782
Gabriel Moura <gabrielmouraintj@gmail.com>
2025-04-01 09:17:29 -0300

    nova versão da primeira task

--- exercicio1.py
+++ exercicio1.py
@@ -1,4 +1,11 @@
 nome = input('Qual é seu nome: ')
 idade = int(input('Qual é sua idade: '))
 salario = float(input('Qual é seu salário: '))
-print(f"Seu nome é: {nome} \nSua idade é: {idade} \n Seu salário é: {salario}")
+aumento = float(input('Qual é seu percentual: '))
+porcentagem = salario * (aumento / 100)
+novo_salario = porcentagem + salario
+print(f"Seu nome é: {nome} \n"
+f"Sua idade é: {idade} anos \n"
+f"Seu salário é: {salario:.2f} \n"
+f"Seu percentual de aumento é: {aumento}\n"
+f"Seu salário atual é: {novo_salario:.2f}")
