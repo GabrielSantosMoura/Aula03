0be8dd598e257e83df65f081e7ff493ac39e9c41
Gabriel Moura <gabrielmouraintj@gmail.com>
2025-04-01 10:42:00 -0300

    quarta task concluida

---
+++ exercicio04.py
@@ -0,0 +1,6 @@
+numb1 = float(input("Digite o primeiro número: "))
+numb2 = float(input("Digite o primeiro número: "))
+if numb1 > numb2:
+    print(f"{numb1} e {numb2}")
+else:
+    print(f"{numb1} e {numb2}")
