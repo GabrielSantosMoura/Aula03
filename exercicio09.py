ff750b2e8bd8d68e62e1ec66f53191a894e71663
Gabriel Moura <gabrielmouraintj@gmail.com>
2025-04-08 09:26:42 -0300

    finalizada a task do dia

---
+++ exercicio09.py
@@ -0,0 +1,29 @@
+month = int(input("Digite um número correspondente ao mês: "))
+
+if month == 1:
+    print("Janeiro")
+elif month == 2:
+    print("Fevereiro")
+elif month == 3:
+    print("Março")
+elif month == 4:
+    print("Abril")
+elif month == 5:
+    print("Maio")
+elif month == 6:
+    print("Junho")
+elif month == 7:
+    print("Julho")
+elif month == 8:
+    print("Agosto")
+elif month == 9:
+    print("Setembro")
+elif month == 10:
+    print("Outubro")
+elif month == 11:
+    print("Novembro")
+elif month == 12:
+    print("Dezembro")
+else:
+    print("valor invalido")
+
